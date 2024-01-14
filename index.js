4b1883f45f6b2871964aedf8f9dee3d592a8a7c2
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2024-01-14 17:53:23 -0300

    Funcionamento do módulo Http (Criação de servidor local)

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-//Aprendendo sobre importação e exportação
-const { Person } = require('./Person');
-const person = new Person('Nome');
+/*
+=========================
+Aprendendo sobre importação e exportação
+=========================
+*/
+// const { Person } = require('./Person');
+// const person = new Person('Nome');
 // console.log(person.sayMyName());
 
 
 // require('./modules/path');
 // require('./modules/fs');
+require('./modules/http')
 
