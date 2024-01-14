587ee948f2fca87b6f3c92991c96956d0d73fb4b
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2024-01-14 19:47:04 -0300

    Inserindo dados no Banco de Dados

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
-/*
-=========================
-Aprendendo sobre importação e exportação
-=========================
-*/
-// const { Person } = require('./Person');
-// const person = new Person('Nome');
-// console.log(person.sayMyName());
 
-
-// require('./modules/path');
-// require('./modules/fs');
-// require('./modules/http')
-// require('./modules/express');
 
 const dotenv = require('dotenv');
 dotenv.config();
 
 const conn = require('./src/database/connect');
-
 conn();
+
+require('./modules/express');
+
+
--- modules/express.js
+++ modules/express.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const UserModel = require('../src/models/user.model')
 
 const app = express();
+
+// Indica que para todas as requisições será utilizada o JSON
+app.use(express.json());
 
 app.get('/home', (req, res) => {
     res.status(200).send('<h1>Página Inicial</h1>')
@@ -14,6 +18,19 @@ app.get('/users', (req, res) => {
          email: "jane@doe.com"}
     ]
     res.status(200).json(users);
+})
+
+app.post('/users', async (req, res) => {
+    try {
+        const user = await UserModel.create(req.body);
+        // O registro foi criado com sucesso
+        res.status(201).json(user);
+
+    }catch(err) {
+        res.status(500).send(err.message);
+
+    }
+    
 })
 
 const port = 8080;
