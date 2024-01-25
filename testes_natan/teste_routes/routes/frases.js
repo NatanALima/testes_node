ff3ab50e81b211bea39ec17f122f108b833c4a7d
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2024-01-24 23:14:00 -0300

    corrigindo o router.get do primeiro exemplo

--- testes_natan/teste_routes/routes/frases.js
+++ testes_natan/teste_routes/routes/frases.js
@@ -3,8 +3,16 @@ const frasesController = require("../controllers/frases");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    frasesController.fraseFazendinha(req, res);
-})
+/* 
+    Há algumas coisas obsoletas neste formato. São elas:
+    1º: não é necessário passar no get do router a função "(req, res) => {}", haja vista que os módulos do arquivo Controller já a possuem
+    2º: não é necessário definir os parametros nas funções referentes ao arquivo controller
+*/
+// router.get("/", (req, res) => {
+//     frasesController.fraseFazendinha(req, res);
+// })
+
+//Forma mais simplificada
+router.get("/", frasesController.fraseFazendinha);
 
 module.exports = router;
